361be51cc117da674c9635a8695b102f64af0189
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-01-13 08:10:02 +0530

    wip commits

--- tech-sample/R/air-pollution-assignment/pollutantmean.R
+++ tech-sample/R/air-pollution-assignment/pollutantmean.R
@@ -1,1 +1,11 @@
-empty file 
+pollutantmean <- function (directory, pollutant, id) {
+
+  for (i in id){
+    filenumber <- sprintf("%03d", i)
+    filename <- paste("specdata/" , filenumber , ".csv" , sep ="")
+    d <- read.csv(filename)
+    
+  }
+  
+}
+
