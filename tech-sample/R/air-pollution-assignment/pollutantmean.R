233501393c154f66a9351d2ceb7444253eeb15f9
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-01-14 18:50:55 +0530

    basic pollutant mean code

--- tech-sample/R/air-pollution-assignment/pollutantmean.R
+++ tech-sample/R/air-pollution-assignment/pollutantmean.R
@@ -1,11 +1,15 @@
 pollutantmean <- function (directory, pollutant, id) {
-
-  for (i in id){
-    filenumber <- sprintf("%03d", i)
-    filename <- paste("specdata/" , filenumber , ".csv" , sep ="")
-    d <- read.csv(filename)
+    sum <- 0
+    count <- 0
     
+    for (i in id){
+        filenumber <- sprintf("%03d", i)
+        filename <- paste("specdata/" , filenumber , ".csv" , sep ="")
+        #print(filename)
+        d <- read.csv(filename)
+        x <- d[!is.na(d$sulfate), ]
+        sum = sum + sum(x[, "sulfate" ])
+        count = count + nrow(x)
   }
-  
+  print (sum/count)  
 }
-
