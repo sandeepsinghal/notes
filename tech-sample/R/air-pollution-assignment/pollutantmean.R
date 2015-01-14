8f837bd930781b67bbf2dbe67907c7c6569579d9
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-01-14 20:30:25 +0530

    committing wip for pollutant

--- tech-sample/R/air-pollution-assignment/pollutantmean.R
+++ tech-sample/R/air-pollution-assignment/pollutantmean.R
@@ -1,13 +1,14 @@
-pollutantmean <- function (directory, pollutant, id) {
+pollutantmean <- function (directory, pollutant = "sulfate", id) {
     sum <- 0
     count <- 0
     
     for (i in id){
         filenumber <- sprintf("%03d", i)
         filename <- paste("specdata/" , filenumber , ".csv" , sep ="")
-        #print(filename)
+        
         d <- read.csv(filename)
-        x <- d[!is.na(d$sulfate), ]
+        x <- d[!is.na(d[,"sulfate"]), ]
+        
         sum = sum + sum(x[, "sulfate" ])
         count = count + nrow(x)
   }
