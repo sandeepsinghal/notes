801c732a6037f0c573b7bc31f780490d7b2fd32b
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-01-15 19:36:36 +0530

    completing the assignment

--- tech-sample/R/air-pollution-assignment/pollutantmean.R
+++ tech-sample/R/air-pollution-assignment/pollutantmean.R
@@ -1,11 +1,25 @@
 pollutantmean <- function (directory, pollutant = "sulfate", id) {
+    if (pollutant == "sulphate"){
+        print("s")
+    }
+    else {
+        print("p")
+    }
+}
+pollutantmean <- function (directory, pollutant = "sulfate", id) {
     data <- data.frame()
     for (i in id){
         filenumber <- sprintf("%03d", i)
-        filename <- paste(directory , filenumber , ".csv" , sep ="")
+        filename <- paste(directory , '/', filenumber , ".csv" , sep ="")
         d <- read.csv(filename)        
         data <- rbind(data,d)
     }
-    m <- mean(data[pollutant], na.rm = TRUE)
-    print(m)
+    pmean <- 0 
+    if (pollutant == "sulfate"){
+        pmean <- mean(data[, "sulfate"], na.rm = TRUE)    
+    }
+    else if (pollutant == "nitrate"){
+        pmean <- mean(data[, "nitrate"], na.rm = TRUE)    
+    }
+    round(pmean,3)
 }
