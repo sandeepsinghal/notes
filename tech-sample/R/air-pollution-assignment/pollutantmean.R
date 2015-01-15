0c96db07b3e671e6672cc24fc2543f837e2f9c00
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-01-15 18:59:20 +0530

    one more version of pollutant mean

--- tech-sample/R/air-pollution-assignment/pollutantmean.R
+++ tech-sample/R/air-pollution-assignment/pollutantmean.R
@@ -1,16 +1,11 @@
 pollutantmean <- function (directory, pollutant = "sulfate", id) {
-    sum <- 0
-    count <- 0
-    
+    data <- data.frame()
     for (i in id){
         filenumber <- sprintf("%03d", i)
-        filename <- paste("specdata/" , filenumber , ".csv" , sep ="")
-        
-        d <- read.csv(filename)
-        x <- d[!is.na(d[,"sulfate"]), ]
-        
-        sum = sum + sum(x[, "sulfate" ])
-        count = count + nrow(x)
-  }
-  print (sum/count)  
+        filename <- paste(directory , filenumber , ".csv" , sep ="")
+        d <- read.csv(filename)        
+        data <- rbind(data,d)
+    }
+    m <- mean(data[pollutant], na.rm = TRUE)
+    print(m)
 }
