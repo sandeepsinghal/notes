98108d60ea06deb936f87159ea37847ba938d056
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-01-16 15:01:20 +0530

    completing part 2 of the assignment

--- tech-sample/R/air-pollution-assignment/complete.R
+++ tech-sample/R/air-pollution-assignment/complete.R
@@ -6,10 +6,9 @@ complete <- function(directory, id = 1:332) {
         fileNumber = sprintf("%03d", i)
         file <- paste(directory, "/", fileNumber , ".csv", sep="")
         x <- read.csv(file)
-        y <- x[!is.na(x$"sulfate") & !is.na(x$"nitrate"),]
-        n <- nrow(y)
+        n <- nrow(x[!is.na(x$"sulfate") & !is.na(x$"nitrate"),])
         cData <- rbind(cData, data.frame(id=i, nobs=n))
     }
     
-    print(cData)
+    cData
 }
