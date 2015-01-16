1315be3710a86c78690fd145ddbd5994e2da4f49
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-01-16 13:28:34 +0530

    fixing an error where a vector was being sent instead of an integer

--- tech-sample/R/air-pollution-assignment/complete.R
+++ tech-sample/R/air-pollution-assignment/complete.R
@@ -1,29 +1,14 @@
 complete <- function(directory, id = 1:332) {
-    ## 'directory' is a character vector of length 1 indicating
-    ## the location of the CSV files
     
-    ## 'id' is an integer vector indicating the monitor ID numbers
-    ## to be used
-    
-    ## Return a data frame of the form:
-    ## id nobs
-    ## 1  117
-    ## 2  1041
-    ## ..
-    ## where 'id' is the monitor ID number and 'nobs' is the
-    ## number of complete cases
-    
-    
-    
-    cData <- data.frame(id="", nobs="")
+    cData <- data.frame(id= integer(), nobs= integer())
     
     for (i in id){
-        fileNumber = sprintf("%03d", id)
+        fileNumber = sprintf("%03d", i)
         file <- paste(directory, "/", fileNumber , ".csv", sep="")
         x <- read.csv(file)
         n <- nrow(x[!is.na(x[,"sulfate"]), ])
-        print (n)
-        #cData <- rbind(cData, data.frame(id=file, nobs=n))
-        #print(cData)
+        cData <- rbind(cData, data.frame(id=i, nobs=n))
     }
+    
+    print(cData)
 }
