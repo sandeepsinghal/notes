aa248603d6a277588b4876e101091ff3560f1167
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-01-16 13:34:31 +0530

    fixing all bugs

--- tech-sample/R/air-pollution-assignment/complete.R
+++ tech-sample/R/air-pollution-assignment/complete.R
@@ -6,7 +6,8 @@ complete <- function(directory, id = 1:332) {
         fileNumber = sprintf("%03d", i)
         file <- paste(directory, "/", fileNumber , ".csv", sep="")
         x <- read.csv(file)
-        n <- nrow(x[!is.na(x[,"sulfate"]), ])
+        y <- x[!is.na(x$"sulfate") & !is.na(x$"nitrate"),]
+        n <- nrow(y)
         cData <- rbind(cData, data.frame(id=i, nobs=n))
     }
     
