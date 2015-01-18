153ff750fda72c609ea409a537ecb851406d09c2
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-01-18 19:22:41 +0530

    fixing the bug

--- tech-sample/R/air-pollution-assignment/corr.R
+++ tech-sample/R/air-pollution-assignment/corr.R
@@ -1,12 +1,12 @@
 corrFile <- function (fileName, threshold = 0) {
     x <- read.csv(fileName)
     
-    cData <- x[!is.na(x$"sulfate") & !is.na(x$"nitrate") & x$"sulfate",]
+    cData <- x[!is.na(x$"sulfate") & !is.na(x$"nitrate"),]
     
     if (nrow(cData) > threshold) {
         sulfate <- cData[, c("sulfate")]
         nitrate <- cData[, c("nitrate")]
-        monitorCor <- cor(sulfate, nitrate)
+        monitorCor <- cor(sulfate, y = nitrate)
         return(round(monitorCor, digits=5))
     }    
 }
@@ -14,14 +14,20 @@ corrFile <- function (fileName, threshold = 0) {
 corr <- function(directory, threshold = 0) {
     files = list.files(directory)
     corVector <- vector(mode = "numeric")
+    fileVector <- vector(mode = "character")
 
     for (file in files){
         fileName <- paste(directory, "/", file, sep ="")
         m <- corrFile(fileName, threshold)
-        corVector <- c(corVector, m)
-        if (fileName == "specdata/020.csv") {
+        if (!is.null(m)) {
+            corVector <- c(corVector, m)
+            fileVector <- c(fileVector, fileName)            
+        }        
+        
+        if (fileName == "specdata/001.csv") {
             #break
         }
+        
     }
     corVector
 }
