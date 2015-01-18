58aa100f42bd980640d818cb2b2982f78773b56a
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-01-18 15:28:00 +0530

    finishing the assignment with one bug

--- tech-sample/R/air-pollution-assignment/corr.R
+++ tech-sample/R/air-pollution-assignment/corr.R
@@ -1,29 +1,20 @@
 corrFile <- function (fileName, threshold = 0) {
     x <- read.csv(fileName)
     
-    cData <- x[!is.na(x$"sulfate") & !is.na(x$"nitrate") & x$"sulfate" > threshold & x$"nitrate" > threshold,]
+    cData <- x[!is.na(x$"sulfate") & !is.na(x$"nitrate") & x$"sulfate",]
     
-    if (nrow(cData) > 1) {
+    if (nrow(cData) > threshold) {
         sulfate <- cData[, c("sulfate")]
         nitrate <- cData[, c("nitrate")]
         monitorCor <- cor(sulfate, nitrate)
-        return(monitorCor)
+        return(round(monitorCor, digits=5))
     }    
 }
 
 corr <- function(directory, threshold = 0) {
-    ## 'directory' is a character vector of length 1 indicating
-    ## the location of the CSV files
-    
-    ## 'threshold' is a numeric vector of length 1 indicating the
-    ## number of completely observed observations (on all
-    ## variables) required to compute the correlation between
-    ## nitrate and sulfate; the default is 0
-    
-    ## Return a numeric vector of correlations
     files = list.files(directory)
     corVector <- vector(mode = "numeric")
-    
+
     for (file in files){
         fileName <- paste(directory, "/", file, sep ="")
         m <- corrFile(fileName, threshold)
