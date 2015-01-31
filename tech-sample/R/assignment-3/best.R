83d463e1bb06309b96bc977397f8f259be744682
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-01-31 13:33:31 +0530

    adding stop function for error handling

--- tech-sample/R/assignment-3/best.R
+++ tech-sample/R/assignment-3/best.R
@@ -2,22 +2,26 @@ best <- function(state, outcome) {
     
     valid_outcomes = c("heart attack","heart failure","pneumonia")
     
-    if (is.element(outcome, valid_outcomes)){
-        print('outcome is present')
-    }
-    else {
-        print('outcome is not present')
+    if (!is.element(outcome, valid_outcomes)){
+        stop('outcome is not present')
     }
     
     outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-    
+
     outcome_state <- outcome[outcome$State == state,]
+    
     nState <- nrow(outcome_state)
     
     if (nState == 0){
-        print('invalid state')
+        stop('invalid state')
     }
-    else {
-        print (nState)
-    }
+    
+    # heart attack : column 11 
+    # Heart failure : column 17
+    # Pneumonia : column 23
+    #outcome[,11] <- as.numeric(outcome[, 11])
+    #outcome[,17] <- as.numeric(outcome[, 17])
+    #outcome[,23] <- as.numeric(outcome[, 23])
+    
+ 
 }
